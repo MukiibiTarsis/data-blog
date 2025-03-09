bde4308a8afe8ca479aae954ae5506d40056b13d
Tarsis Mukiibi <tarsismukiibi@gmail.com>
2025-03-09 13:59:28 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ let second_number = 7;
 let sum = first_number + second_number;
 
 return (
-    <div> 
+    <div style = {{color : 'red', backgroundColor: '#f9f9f9', border: '5px solid px', padding:'30px', 
+      margin: '50px', borderRadius:'5px' 
+    }}> 
       <h1> DATA SCIENCE FOR AFRICA</h1>
       <h2> An initiative for 2025</h2>
       <p> Welcome to our website</p>
